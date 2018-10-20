61a3e1b6ccb15898d7d14529a47062e4f0af7d23
Nick Thorp <nickthorp20@gmail.com>
2018-10-19 23:17:08 -0500

    Adding in some comments!

--- build/build.sh
+++ build/build.sh
@@ -3,4 +3,5 @@ export PATH=$PATH:~/bin
 
 echo "Converting Sass files to CSS..."
 bundle exec sass ../_sass/main.scss ../main.css
+bundle exec sass ../_sass/contact.scss ../contact.css
 echo "Conversion complete."
